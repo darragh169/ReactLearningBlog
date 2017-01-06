d09084118e6c2c0ce6ffae0b348ea422d1973685
darragh169 <darragh169@gmail.com>
2017-01-05 19:35:29 -0800

    Initial Routes added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 //index.js
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import './css/style.css';
 
 import BlogEntrance from './components/BlogEntrance';
 import App from './components/App';
+import NotFound from './components/NotFound';
 
-render(<App />, document.getElementById('main'));
+const Root = () => {
+	return(
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={App}></Match>
+				<Miss component={NotFound}></Miss>
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root />, document.getElementById('main'));
